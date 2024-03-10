3320dbde72cc01d9dd7e51babebd60a72ce04255
ObserverOfTime <chronobserver@disroot.org>
2024-03-10 20:23:32 +0200

    build: update bindings & bump version

---
+++ Package.swift
@@ -0,0 +1,46 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TreeSitterVimdoc",
+    platforms: [.macOS(.v10_13), .iOS(.v11)],
+    products: [
+        .library(name: "TreeSitterVimdoc", targets: ["TreeSitterVimdoc"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "TreeSitterVimdoc",
+                path: ".",
+                exclude: [
+                    "Cargo.toml",
+                    "Makefile",
+                    "binding.gyp",
+                    "bindings/c",
+                    "bindings/go",
+                    "bindings/node",
+                    "bindings/python",
+                    "bindings/rust",
+                    "prebuilds",
+                    "grammar.js",
+                    "package.json",
+                    "package-lock.json",
+                    "pyproject.toml",
+                    "setup.py",
+                    "test",
+                    "examples",
+                    ".editorconfig",
+                    ".github",
+                    ".gitignore",
+                    ".gitattributes",
+                ],
+                sources: [
+                    "src/parser.c",
+                ],
+                resources: [
+                    .copy("queries")
+                ],
+                publicHeadersPath: "bindings/swift",
+                cSettings: [.headerSearchPath("src")])
+    ],
+    cLanguageStandard: .c11
+)
--- binding.gyp
+++ binding.gyp
@@ -2,18 +2,19 @@
   "targets": [
     {
       "target_name": "tree_sitter_vimdoc_binding",
+      "dependencies": [
+        "<!(node -p \"require('node-addon-api').targets\"):node_addon_api_except",
+      ],
       "include_dirs": [
-        "<!(node -e \"require('nan')\")",
-        "src"
+        "src",
       ],
       "sources": [
         "bindings/node/binding.cc",
         "src/parser.c",
-        # If your language uses an external scanner, add it here.
       ],
       "cflags_c": [
-        "-std=c99",
-      ]
+        "-std=c11",
+      ],
     }
   ]
 }
