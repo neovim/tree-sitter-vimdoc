d74fa5fe4591f9d9d7888c170aec56b5e1fec019
Christian Clason <c.clason@uni-graz.at>
2025-05-03 12:09:31 +0200

    feat(parser)!: update to ABI 15

--- src/parser.c
+++ src/parser.c
@@ -14,7 +14,7 @@
 #pragma GCC optimize ("O0")
 #endif
 
-#define LANGUAGE_VERSION 14
+#define LANGUAGE_VERSION 15
 #define STATE_COUNT 115
 #define LARGE_STATE_COUNT 17
 #define SYMBOL_COUNT 95
@@ -10770,7 +10770,7 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0},
   [1] = {.lex_state = 31},
   [2] = {.lex_state = 31},
@@ -15799,6 +15799,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_vimdoc(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
+    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -15816,6 +15817,13 @@ TS_PUBLIC const TSLanguage *tree_sitter_vimdoc(void) {
     .lex_modes = (const void*)ts_lex_modes,
     .lex_fn = ts_lex,
     .primary_state_ids = ts_primary_state_ids,
+    .name = "vimdoc",
+    .max_reserved_word_set_size = 0,
+    .metadata = {
+      .major_version = 3,
+      .minor_version = 0,
+      .patch_version = 1,
+    },
   };
   return &language;
 }
