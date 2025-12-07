1cf949678153b63a280dabff92567752563d65f4
Justin M. Keyes <justinkz@gmail.com>
2025-12-06 21:00:33 -0500

    release v4.0.2

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION := 4.0.1
+VERSION := 4.0.2
 
 LANGUAGE_NAME := tree-sitter-vimdoc
 
--- src/parser.c
+++ src/parser.c
@@ -17079,7 +17079,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_vimdoc(void) {
     .metadata = {
       .major_version = 4,
       .minor_version = 0,
-      .patch_version = 1,
+      .patch_version = 2,
     },
   };
   return &language;
